a64a9ae5b6adb18b8a620dfc0ad465b32be4e9fd
Chester Vanhuang <chestervanhuang@gmail.com>
2020-01-23 11:08:14 -0800

    data api routes

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+var express     = require('express'),
+    router      = express.Router(),
+    db          = require('../models'),
+    dataHelpers = require('../helpers/data');
+    // statsHelpers = require('../helpers/stats'),
+    // settingsHelpers = require('../helpers/settings');
+
+router.route('/data')
+    .get(dataHelpers.getData)
+    .post(dataHelpers.createData)
+    .delete(dataHelpers.deleteData);
+
+// router.route('/data/:id')
+//     .get()
+//     .put()
+//     .delete();
+
+// router.route('/settings')
+//     .get()
+//     .put();
+
+// router.route('/statistics')
+//     .get()
+//     .put()
+//     .delete();
+
+module.exports = router;
+
+    
+
+
